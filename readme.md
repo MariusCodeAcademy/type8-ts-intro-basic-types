40a7edc7020686c98a7b2ba7c2027c0a4c553652
Marius <marius@example.com>
2022-07-07 08:49:58 +0300

    init

---
+++ readme.md
@@ -0,0 +1,8 @@
+# typescript
+
+1. npm i typescript
+2. tsc app.js
+3. tsc --init
+4. "outDir": "./public/js",
+5. "rootDir": "./src",
+6. tsc --watch
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+// implicit type
+let username = 'James';
+// expilic type
+let lastName: string = 'Bond';
+// lastName = 5;
+
+let age: number;
+age = 28;
