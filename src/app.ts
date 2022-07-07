9f4bbf9783957cf4f9b9538076835d9b03282145
Marius <marius@example.com>
2022-07-07 08:57:02 +0300

    basic types

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
+// Basic types =========
+
 // implicit type
-let username = 'James';
+let username: string = 'James';
 // expilic type
 let lastName: string = 'Bond';
 // lastName = 5;
 
 let age: number;
-age = 28;
+// age = '28';
+
+let isAdmin: boolean = false;
+// isAdmin = 0;
+
+let password: null = null;
+// password = 0;
+
+let email: undefined = undefined;
